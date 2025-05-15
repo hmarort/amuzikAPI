5baa47bb433c98ebbcbe12c7f68b78960b64d0b4
hmarort <hecmaror04@gmail.com>
2025-05-15 08:51:31 +0200

    Modificacions

--- app/Controllers/FriendController.php
+++ app/Controllers/FriendController.php
@@ -1,38 +1,55 @@
 <?php
-
 namespace App\Controllers;
-
 use CodeIgniter\HTTP\ResponseInterface;
-
 class FriendController extends BaseController
 {
     private $token = 'W66jQhYGGzEIuCcAXfpTJkt7uH6GBGpcJLCSXo6O2WF1AZkxiMXpypFaKEfA';
-
+    
     public function saveFriendship(){
-
         $jsonBody = $this->request->getJSON();
-
         if ($jsonBody) {
+            $user_id = $jsonBody->user_id;
+            $friend_id = $jsonBody->friend_id;
+            
+            if ($user_id < $friend_id) {
+                $user1 = $user_id;
+                $user2 = $friend_id;
+            } else {
+                $user1 = $friend_id;
+                $user2 = $user_id;
+            }
+            
             $this->friendModel->save([
-                'user1'=>$jsonBody->user_id,
-                'user2'=>$jsonBody->friend_id,
+                'user1' => $user1,
+                'user2' => $user2,
             ]);
-
             return $this->response->setJSON(['status' => 'success']);
         } else {
             return $this->response->setJSON(['status' => 'error', 'message' => 'Invalid input']);
         }
     }
-
+    
     public function deleteFriendship()
     {
         $jsonBody = $this->request->getJSON();
         if (!$jsonBody) {
             return $this->response->setJSON(['status' => 'error', 'message' => 'Invalid input']);
         }
-        $friendId = $jsonBody->friend_id;
-        $userId = $jsonBody->user_id;   
-        $friendship = $this->friendModel->where('user1', $userId)->where('user2', $friendId)->first();
+        
+        $user_id = $jsonBody->user_id;
+        $friend_id = $jsonBody->friend_id;
+        
+        $friendship = null;
+        if ($user_id < $friend_id) {
+            $friendship = $this->friendModel->where('user1', $user_id)
+                                         ->where('user2', $friend_id)
+                                         ->first();
+        } else {
+            $friendship = $this->friendModel->where('user1', $friend_id)
+                                         ->where('user2', $user_id)
+                                         ->first();
+        }
+        
         if ($friendship) {
             $this->friendModel->delete($friendship['id']);
             return $this->response->setJSON(['status' => 'success']);
@@ -40,5 +57,4 @@ class FriendController extends BaseController
             return $this->response->setJSON(['status' => 'error', 'message' => 'Friendship not found']);
         }
     }
-
 }
